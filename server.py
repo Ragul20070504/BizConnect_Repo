9704bd3dfe5ffe089072a55e1fb840015785a1c0
Ragul20070504 <ragul20070504@gmail.com>
2025-10-19 16:35:49 +0530

    Initial commit

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ def translate_text(text, dest_language='ta'):
     except Exception as e:
         return f"Translation Error: {str(e)}"
 
-def read_file_content(file):
+def read_file_content(file):cd
     filename = file.filename.lower()
     
     if filename.endswith('.txt'):
